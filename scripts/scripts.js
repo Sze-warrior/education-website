8e2224b39e2cb8c7430e4d65c9f4d0512df897f7
Michael Szeto <szetomj@gmail.com>
2018-04-21 23:57:58 +1200

    Broke up styling and made up font sizes
    Broke up styling and made up font sizes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
     var scroll_pos = 0;
     var header = $('.header-container');
     var headerLogo = $('.header-logo');
-    var intro = $('.intro');
+    var homeIntro = $('.intro');
 
     $(document).scroll(function() { 
         scroll_pos = $(this).scrollTop();
         
         if(scroll_pos > 300) {
-        	$(intro).addClass('show');
+        	$(homeIntro).addClass('show');
         	$(headerLogo).addClass('black');
         	$(header).addClass('white');
     	}else{
